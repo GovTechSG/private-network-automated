e9dc51230e2b350cd5fa20a548c15888471309cb
Yong Wen Chua <lawliet89@users.noreply.github.com>
2018-03-21 11:05:50 +0800

    Fix time server related issues

--- provision/terraform/main.tf
+++ provision/terraform/main.tf
@@ -91,7 +91,7 @@ resource "aws_security_group_rule" "out" {
     type            = "egress"
     from_port       = 0
     to_port         = 65535
-    protocol        = "tcp"
+    protocol        = "all"
     cidr_blocks     = ["0.0.0.0/0"]
 
     security_group_id = "${aws_security_group.nodes.id}"
